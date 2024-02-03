d7f12655a83376132904b30e01fa5542ff46f711
danellopes <daniel.nunes.meira@outlook.com>
2024-02-03 13:06:40 -0300

    #Inner Factories

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace C_InnerFactory;
+
+public class Point
+{
+    private double x, y;
+
+    private Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Creates new object every time it's called
+    // public static Point Origin => new Point(0, 0);
+
+    // Initiates object once and returns every time its called
+    public static Point Origin = new Point(0, 0);
+
+    public static class Factory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"x: {x}, y: {y}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var polarPoint = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+        System.Console.WriteLine(polarPoint);
+        var cartesianPoint = Point.Factory.NewCartesianPoint(1.0, 2.0);
+        System.Console.WriteLine(cartesianPoint);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Example of the Inner Factory
+
+This example of the faceted builder design pattern was develop using C# language.
+
+This technique allows is to make use of the Factory, without making our constructor publict. To do this, we make the Factory class into an Inner class, or Inner factory. This way, it call access the private constructor and still serves as a more organized way to initiate an object.
+
+If you're interested in the [udemy course](https://www.udemy.com/course/design-patterns-csharp-dotnet) by [Dmitri Nesteruk](https://www.udemy.com/user/dmitrinesteruk/).
